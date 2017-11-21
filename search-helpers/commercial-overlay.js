e60460f5cb7c5c35b1a0a456361df9431230ab1b
Chris Whong <chris@whong.org>
2017-11-20 22:08:25 -0500

    refactor commercial overlay search helper

--- search-helpers/commercial-overlay.js
+++ search-helpers/commercial-overlay.js
@@ -1,19 +1,17 @@
 const carto = require('../utils/carto');
 
-const commercialOverlay = (string) => {
-  const SQL = `
-    SELECT DISTINCT overlay, cartodb_id
-    FROM support_zoning_co
-    WHERE LOWER(overlay) LIKE LOWER('%25${string.toLowerCase()}%25')
-    LIMIT 5
-  `;
+const overlays = ['C1-1', 'C1-2', 'C1-3', 'C1-4', 'C1-5', 'C2-1', 'C2-2', 'C2-3', 'C2-4', 'C2-5'];
 
-  return carto.SQL(SQL).then(rows =>
-    rows.map((row) => {
-      row.label = row.overlay;
-      row.type = 'commercial-overlay';
-      return row;
+const commercialOverlay = (string) => {
+
+  return new Promise((resolve) => {
+    const matches = overlays.filter(overlay => overlay.toLowerCase().indexOf(string.toLowerCase()) !== -1);
+    const results = matches.map(result => ({
+      label: result,
+      type: 'commercial-overlay',
     }));
+    resolve(results);
+  });
 };
 
 module.exports = commercialOverlay;
