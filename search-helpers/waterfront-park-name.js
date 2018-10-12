3714067893f66bbbf2d062b3dc365683f337746d
trbmcginnis <taylormcgin@gmail.com>
2018-10-12 11:10:04 -0400

    update with more search terms

--- search-helpers/waterfront-park-name.js
+++ search-helpers/waterfront-park-name.js
@@ -2,11 +2,15 @@ const carto = require('../utils/carto');
 
 const waterfrontParkName = (string) => {
   const SQL = `
-    SELECT park_name, ST_AsGeoJSON(the_geom) as the_geom
-    FROM wpaas_v201810
-    WHERE LOWER(park_name) LIKE LOWER('%25${string.toLowerCase()}%25')
-    ORDER BY park_name ASC
+    SELECT paws_name, description, adjacent_waterway, location
+    FROM pawsmastertable
+    WHERE LOWER(paws_name) LIKE LOWER('%25${string.toLowerCase()}%25')
+    ORDER BY paws_name ASC
     LIMIT 5
+    LEFT JOIN
+    (SELECT ST_AsGeoJSON(the_geom) as the_geom, paws_id
+    FROM wpaas_v201810)
+    ON paws_id = paws_id
   `;
 
   return carto.SQL(SQL).then(rows =>
