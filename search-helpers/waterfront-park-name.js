dc65b44751468ee914ab22f90d502538c37f8b1b
allthesignals <wilburnforce@gmail.com>
2018-10-12 11:31:35 -0400

    Fix SQL query; remove geom parsing; add display label

--- search-helpers/waterfront-park-name.js
+++ search-helpers/waterfront-park-name.js
@@ -2,20 +2,19 @@ const carto = require('../utils/carto');
 
 const waterfrontParkName = (string) => {
   const SQL = `
-    SELECT paws_name, description, adjacent_waterway, location
+    SELECT paws_name, paws_id, description, adjacent_waterway, location
     FROM pawsmastertable
     WHERE LOWER(paws_name) LIKE LOWER('%25${string.toLowerCase()}%25')
-    ORDER BY paws_name ASC
-    LIMIT 5
-    LEFT JOIN
-    (SELECT ST_AsGeoJSON(the_geom) as the_geom, paws_id
-    FROM wpaas_v201810)
-    ON paws_id = paws_id
+      OR LOWER(description) LIKE LOWER('%25${string.toLowerCase()}%25')
+      OR LOWER(adjacent_waterway) LIKE LOWER('%25${string.toLowerCase()}%25')
+      OR LOWER(location) LIKE LOWER('%25${string.toLowerCase()}%25')
   `;
+
+  console.log(SQL);
 
   return carto.SQL(SQL).then(rows =>
     rows.map((row) => {
-      row.the_geom = JSON.parse(row.the_geom);
+      row.label = row.paws_name;
       row.type = 'waterfront-park-name';
       return row;
     }));
